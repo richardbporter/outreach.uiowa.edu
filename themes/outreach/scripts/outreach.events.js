5ed7abeadc3dd4ff382bddec8f6e3d8277290cfa
Richard B. Porter <richard-b-porter@uiowa.edu>
2012-09-26 10:29:39 -0500

    start outreach script

---
+++ themes/outreach/scripts/outreach.events.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * 
+ *  This file holds all the jQuery for the Outreach site.
+ */
+
+(function ($)) {
+ $(document).ready(function() {
+   // Do something. 
+   (function doSomething() {
+   });
+ });
+})(jQuery);
