680aa893a2fe1d24889374f6597d4bbd37a6c0ea
Richard B. Porter <richard-b-porter@uiowa.edu>
2012-10-01 13:47:37 -0500

    print map div on front page

--- themes/outreach/templates/page--front.tpl.php
+++ themes/outreach/templates/page--front.tpl.php
@@ -128,7 +128,7 @@
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-      <?php print render($page['content']); ?>
+      <div id="map"></div>
       <?php print $feed_icons; ?>
     </div></div> <!-- /.section, /#content -->
 
