d9077dc96217c57cac1f37d61500f731304c2f27
Richard B. Porter <richard-b-porter@uiowa.edu>
2012-10-01 13:46:36 -0500

    add javascript resources

---
+++ themes/outreach/js/outreach.events.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * 
+ * This file holds all the jQuery for the Outreach site.
+ */
+
+(function($) {
+  $().ready(function() {
+    mapbox.auto('map', 'richardbporter.County-Alumni');
+  });
+})(jQuery);
