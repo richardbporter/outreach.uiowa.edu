26eb2e60ca28355e444bb4593eb9c189b40ecb47
Richard B. Porter <richard-b-porter@uiowa.edu>
2012-10-16 11:07:19 -0500

    print content on front page

--- themes/outreach/templates/page--front.tpl.php
+++ themes/outreach/templates/page--front.tpl.php
@@ -125,7 +125,7 @@
     <?php if ($action_links): ?>
       <ul class="action-links"><?php print render($action_links); ?></ul>
     <?php endif; ?>
-    
+    <?php print render($page['content']); ?>
     <div id="map"></div>
     
   </div></div> <!-- /.section, /#content -->
