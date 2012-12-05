734f9069d78bc9cc7812041d26dbcd7012e394cf
Richard B. Porter <richard-b-porter@uiowa.edu>
2012-12-05 12:41:35 -0600

    don't print tabs on map pages

--- themes/outreach/templates/page--map.tpl.php
+++ themes/outreach/templates/page--map.tpl.php
@@ -192,29 +192,7 @@
 
             <?php print render($title_prefix); ?>
 
-            <?php if ($title || $primary_local_tasks || $secondary_local_tasks || $action_links = render($action_links)): ?>
-              <header<?php print $content_header_attributes; ?>>
 
-                <?php if ($primary_local_tasks || $secondary_local_tasks || $action_links): ?>
-                  <div id="tasks">
-
-                    <?php if ($primary_local_tasks): ?>
-                      <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?></ul>
-                    <?php endif; ?>
-
-                    <?php if ($secondary_local_tasks): ?>
-                      <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul>
-                    <?php endif; ?>
-
-                    <?php if ($action_links = render($action_links)): ?>
-                      <ul class="action-links clearfix"><?php print $action_links; ?></ul>
-                    <?php endif; ?>
-
-                  </div>
-                <?php endif; ?>
-
-              </header>
-            <?php endif; ?>
 
             <!-- Begin map. -->
             <div id="map">
