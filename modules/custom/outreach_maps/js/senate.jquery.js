cc4c7d6dc6063b88e3ea0e1930e1a5eb1acbd884
Richard B. Porter <richard-b-porter@uiowa.edu>
2012-12-10 11:01:09 -0600

    reduce zoom range, add senate clusters and jump links

--- modules/custom/outreach_maps/js/senate.jquery.js
+++ modules/custom/outreach_maps/js/senate.jquery.js
@@ -25,8 +25,13 @@
 
     // Basic map configuration.
     map.center({ lat: 41.9842807, lon: -93.5697204 });
-    map.setZoomRange(6, 12);
+    map.setZoomRange(7, 12);
     map.zoom(8, true);
+
+    // Create an array of senate districts that are close together.
+    var clustered = [
+      '16', '17', '18', '19', '20', '21', '22', '30', '31', '33', '34', '35', '43', '45', '47'
+    ];
 
     // Add the senate layer.
     map.addLayer(mapbox.layer().id('uiowa-its.iowa-senate-districts'));
@@ -41,6 +46,10 @@
       var senateLink = document.createElement('a');
       $(senateLink).addClass('senate-marker use-ajax');
       $(senateLink).addClass('senate-' + f.properties.text);
+       // If this district is clustered.
+      if ($.inArray(f.properties.text, clustered) != -1) {
+        $(senateLink).addClass('cluster');
+      }
       $(senateLink).text(f.properties.text);
       $(senateLink).attr('href', '/outreach-maps/senate/' + f.properties.text);
 
@@ -77,13 +86,70 @@
       return o;
     });
 
-    // Reduce font size for zoom level 7.
+    // Create cluster marker layer.
+    var clusterMarkers = mapbox.markers.layer().features([{
+      'geometry': {'type': 'Point', 'coordinates': [-93.8697204, 41.77842807]},
+      'properties': { 'text': 'polk' }
+    }, {
+      'geometry': {'type': 'Point', 'coordinates': [-92.3060589, 42.4728884]},
+      'properties': { 'text': 'black-hawk' }
+    }, {
+      'geometry': {'type': 'Point', 'coordinates': [-91.5887572, 41.6687272]},
+      'properties': { 'text': 'johnson' }
+    }, {
+      'geometry': {'type': 'Point', 'coordinates': [-91.5976735, 42.0779506]},
+      'properties': { 'text': 'linn' }
+    }, {
+      'geometry': {'type': 'Point', 'coordinates': [-90.6222899, 41.6416792]},
+      'properties': { 'text': 'scott' }
+    }]).factory(function(f) {
+      var clusterLink = document.createElement('a');
+      $(clusterLink).addClass('cluster-marker cluster-marker-' + f.properties.text);
+      $(clusterLink).attr('href', '');
+      return clusterLink;
+    });
+
+    // Add cluster markers layer.
+    map.addLayer(clusterMarkers);
+
+    // Cluster click handlers.
+    $('#polk, .cluster-marker-polk').click(function(e) {
+      e.preventDefault();
+      map.ease.location({ lat: 41.6842807, lon: -93.5697204 }).zoom(11).optimal();
+    });
+
+    $('#black-hawk, .cluster-marker-black-hawk').click(function(e) {
+      e.preventDefault();
+      map.ease.location({ lat: 42.4728884, lon: -92.3060589 }).zoom(11).optimal();
+    });
+
+    $('#johnson, .cluster-marker-johnson').click(function(e) {
+      e.preventDefault();
+      map.ease.location({ lat: 41.6687272, lon: -91.5887572 }).zoom(11).optimal();
+    });
+
+    $('#linn, .cluster-marker-linn').click(function(e) {
+      e.preventDefault();
+      map.ease.location({ lat: 42.0779506, lon: -91.5976735 }).zoom(11).optimal();
+    });
+
+    $('#scott, .cluster-marker-scott').click(function(e) {
+      e.preventDefault();
+      map.ease.location({ lat: 41.6416792, lon: -90.6222899 }).zoom(11).optimal();
+    });
+
+     // Perform various actions on the zoom event.
     map.addCallback("zoomed", function(map, zoomOffset) {
+      // Get the target zoom level.
       var z = Math.round(map.zoom());
-      if (zoomOffset == -1 && z == 7) {
-        $('.marker').addClass('seven');
+
+      // @TODO: Currently not using cluster markers. Add back in if client wants.
+      if (z >= 10) {
+        $('.cluster-marker').fadeOut();
+        $('.cluster').fadeIn();
       } else {
-        $('.marker').removeClass('seven');
+        $('.cluster').fadeOut();
+        $('.cluster-marker').fadeIn();
       }
     });
 
