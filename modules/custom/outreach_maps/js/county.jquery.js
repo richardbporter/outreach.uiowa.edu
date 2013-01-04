67cb8d30e6894ad46a2068b56248471ff1350093
Richard B. Porter <richard-b-porter@uiowa.edu>
2013-01-04 15:03:01 -0600

    deploy

--- modules/custom/outreach_maps/js/county.jquery.js
+++ modules/custom/outreach_maps/js/county.jquery.js
@@ -92,11 +92,11 @@
         // Trigger the response.
         Drupal.ajax['outreach_maps_county_ajax_action'].specifiedResponse();
 
-        // // Center map.
-        // map.ease.location({
-        //   lat: f.geometry.coordinates[1] + 0.4, // Adjust for smaller viewport.
-        //   lon: f.geometry.coordinates[0]
-        // }).zoom(map.zoom()).optimal();
+        // Center map.
+        map.ease.location({
+          lat: f.geometry.coordinates[1] + 0.4, // Adjust for smaller viewport.
+          lon: f.geometry.coordinates[0]
+        }).zoom(map.zoom()).optimal();
       });
 
       return countyLink;
