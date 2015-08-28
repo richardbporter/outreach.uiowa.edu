4ca615f849ccd3e58b316009d35c7a0fece2e3f5
Mark Ahrens <mark-ahrens@uiowa.edu>
2015-08-28 10:28:15 -0500

    Removing the page title code

--- themes/outreach2/template.php
+++ themes/outreach2/template.php
@@ -9,15 +9,3 @@
 function outreach2_field_collection_view($variables) {
   return '<div>'. $variables['element']['#children'] . '</div>';
 }
-
-/**
- * Implements hook_preprocess_page().
- */
-
-function outreach2_preprocess_page(&$variables) {
-  if(isset($variables['node'])){
-    if($variables['node']->type == 'county'){
-      $variables['title'] = "Iowa Impact: ".$variables['node']->title. " County";
-    }
-  }
-}
