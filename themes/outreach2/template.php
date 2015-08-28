2b32dc93ba99f401f182c402656dbd5a11519390
Mark Ahrens <mark-ahrens@uiowa.edu>
2015-08-28 10:07:50 -0500

    Fixing the county heading styling, adding the title rewrite

--- themes/outreach2/template.php
+++ themes/outreach2/template.php
@@ -9,3 +9,15 @@
 function outreach2_field_collection_view($variables) {
   return '<div>'. $variables['element']['#children'] . '</div>';
 }
+
+/**
+ * Implements hook_preprocess_node().
+ */
+
+function outreach2_preprocess_page(&$variables) {
+  if(isset($variables['node'])){
+    if($variables['node']->type === 'county'){
+      $variables['title'] = "Iowa Impact: ".$variables['node']->title. " County";
+    }
+  }
+}
